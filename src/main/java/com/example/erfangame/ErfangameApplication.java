c8bf08c61b04ad4ab5cab9b4f26f43e32f1d9606
ErfAn Pezeshkpour <www.erfan01724@gmail.com>
2021-08-04 15:33:45 +0430

    Initial commit

---
+++ src/main/java/com/example/erfangame/ErfangameApplication.java
@@ -0,0 +1,13 @@
+package com.example.erfangame;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ErfangameApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ErfangameApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/erfangame/ErfangameApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.erfangame;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ErfangameApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
