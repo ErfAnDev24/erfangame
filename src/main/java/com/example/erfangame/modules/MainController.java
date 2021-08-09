34dcdba2211494747c232cbcd31715cd9575d3d8
ErfAn Pezeshkpour <www.erfan01724@gmail.com>
2021-08-09 20:00:27 +0430

    create MainController & usersController files for showing users.html file

---
+++ src/main/java/com/example/erfangame/modules/MainController.java
@@ -0,0 +1,13 @@
+package com.example.erfangame.modules;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class MainController {
+
+    @RequestMapping("/")
+    public String showIndex(){
+        return "index";
+    }
+}
---
+++ src/main/java/com/example/erfangame/modules/users/controller/usersController.java
@@ -0,0 +1,16 @@
+package com.example.erfangame.modules.users.controller;
+
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+@Controller
+@RequestMapping("/users")
+public class usersController {
+
+    @RequestMapping(value = "" , method = RequestMethod.GET)
+    public String showUsers(){
+        return "users/users";
+    }
+}
